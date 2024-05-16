7db0d1b88c81160532168f939c8a42e420c097f2
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-05-16 19:22:09 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,24 @@
 variable "t_vm_description" {
   description = "Nutanix VM description"
-  type = string
+  type        = string
   sensitive   = false
-  default  = "<%=customOptions['inp_description']%>"
 }
 
-variable "t_ipv4_address" {
-  description = "IPv4 van VM"
-  type = string
-  sensitive   = false
-}
+# variable "t_ipv4_address" {
+#   description = "IPv4 van VM"
+#   type        = string
+#   sensitive   = false
+# }
 
 variable "t_vm_name" {
-  description = "Nutanix VM name van VM in CAPITALS"
-  type = string
+  description = "Nutanix VM name"
+  type        = string
   sensitive   = false
 }
 
 variable "t_hostname" {
-  description = "hostnaam van VM"
-  type = string
+  description = "hostname"
+  type        = string
   sensitive   = false
 }
 
@@ -29,21 +28,6 @@ variable "subnet_name" {
   sensitive   = false
 }
 
-variable "t_ipv4_gateway" {
-  description = "IPv4 gateway van VM"
-  type = string
-}
-
-variable "t_ipv4_mask" {
-  description = "IPv4 subnetmask van VM"
-  type = string
-}
-
-variable "t_ipv4_maskbits" {
-  description = "IPv4 subnetmaskbits van VM"
-  type = string
-}
-
 variable "nutanix_imagename" {
   description = "Name of image for VM"
   type        = string
@@ -51,73 +35,45 @@ variable "nutanix_imagename" {
 }
 
 variable "t_num_vcpus_per_socket" {
-  description = "Nutanix VM vCores per socket, laat deze op 1 staan"
-  type = number
-  default = "1"
+  description = "Nutanix VM vCores per socket"
+  type        = string
+  default     = "1"
 }
 
 variable "t_num_sockets" {
   description = "Nutanix VM vCPU's"
-  type = number
-  default = "<%=customOptions['inp_vcpu']%>"
+  type        = string
 }
 
 variable "t_memory_size_mib" {
   description = "Nutanix VM vMEM"
-  type = number
-  default = "<%=customOptions['inp_vmem']%>"
+  type        = string
 }
 
 variable "t_disk_2_size" {
   description = "Nutanix VM data disk 2"
-  type = string
+  type        = number
 }
 
 variable "t_boot_type" {
   description = "Nutanix VM Boottype"
-  type = string
-  default = "UEFI"
-}
-
-variable "t_ipv4_nameservers" {
-  description = "IPv4 nameservers van VM"
-  type = string
-}
-
-variable "t_ntpserver" {
-  description = "NTP Server"
-  type = string
-}
-
-variable "t_domain" {
-  description = "IPv4 search domein van VM"
-  type = string
-}
-
-variable "vm_domain" {
-  description = "Name of domain for VMs"
   type        = string
-  sensitive   = false
+  default     = "UEFI"
 }
+
 
 # Windows Authentication
 variable "t_admin_username" {
-  description = "Name of domain for VMs"
+  description = "Admin user"
   type        = string
   sensitive   = true
   default     = "Administrator"
 }
 
 variable "t_admin_password" {
-  description = "Name of domain for VMs"
+  description = "Admin pass"
   type        = string
   sensitive   = true
-}
-
-variable "t_admin_unenc" {
-  description = "Unencrypted pwd"
-  type = string
-  sensitive = true
 }
 
 # Nutanix cluster definitie
@@ -133,12 +89,6 @@ variable "nutanix_cluster" {
   sensitive   = false
 }
 
-variable "nutanix_storagecontainer_uuid" {
-  description = "Name of Storage Container" 
-  type        = string
-  sensitive   = false
-}
-
 # redelijk statisch vanaf hier ;-)
 variable "nutanix_username" {
   description = "Nutanix user"
@@ -148,12 +98,6 @@ variable "nutanix_username" {
 
 variable "nutanix_password" {
   description = "Nutanix password"
-  type        = string
-  sensitive   = true
-}
-
-variable "aap_token" {
-  description = "AAP token"
   type        = string
   sensitive   = true
 }
